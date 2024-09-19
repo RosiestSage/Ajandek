f88882efec8c041c765a2bc50d2cd56580025871
Páli Dávid <pali.david@students.jedlik.eu>
2024-09-19 22:49:20 +0200

    kepek

--- david.js
+++ david.js
@@ -1,4 +1,12 @@
-function szog(){
-    sz = Math.random(-30, 30)
-    return sz
+/*
+x = Math.random() *50-25;
+document.getElementsById('rotate1').style.rotate = x + 'deg';
+x = Math.random() *50-25;
+document.getElementById('rotate2').style.rotate = x + 'deg';
+x = Math.random() *50-25;
+document.getElementById('rotate3').style.rotate = x + 'deg';
+*/
+
+function getid() {
+    list = []
 }
